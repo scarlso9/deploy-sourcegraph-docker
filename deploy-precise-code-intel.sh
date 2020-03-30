d56f96c132e3dd621c1277a7aca8a6076e49f58d
Eric Fritz <eric@eric-fritz.com>
2020-03-30 09:28:21 -0500

    Rename lsif-server service.

--- deploy-precise-code-intel.sh
+++ deploy-precise-code-intel.sh
@@ -2,16 +2,16 @@
 set -e
 source ./replicas.sh
 
-# Description: LSIF HTTP server for code intelligence.
+# Description: Precise code intelligence services for Sourcegraph
 #
 # Disk: 200GB / persistent SSD
 # Network: 100mbps
-# Liveness probe: HTTP GET http://lsif-server:3186/healthz
+# Liveness probe: HTTP GET http://precise-code-intel:3186/healthz
 # Ports exposed to other Sourcegraph services: 3186/TCP (server) 9090/TCP (prometheus)
 # Ports exposed to the public internet: none
 #
 docker run --detach \
-    --name=lsif-server \
+    --name=precise-code-intel \
     --network=sourcegraph \
     --restart=always \
     --cpus=2 \
@@ -27,4 +27,4 @@ docker run --detach \
     -v ~/sourcegraph-docker/lsif-server-disk:/lsif-storage \
     index.docker.io/sourcegraph/lsif-server:3.14.0-1@sha256:ed683d2579d807458a892eab2f88fa5618b82a1ae7af02d5e7de1af3f6eea4a7
 
-echo "Deployed lsif-server service"
+echo "Deployed precise-code-intel service"
